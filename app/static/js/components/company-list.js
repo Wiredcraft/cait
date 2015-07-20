dfe1091b26faf30e3b36225a956d436b964fd104
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2015-07-20 14:58:55 +0300

    Basic company filtering by name/country/sector

--- app/static/js/components/company-list.js
+++ app/static/js/components/company-list.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Loader from 'react-loader';
 import { Link } from 'react-router';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Input, ListGroup, ListGroupItem, DropdownButton, MenuItem } from 'react-bootstrap';
 
 import { APIClient } from 'apiclient.js';
 
@@ -37,6 +37,15 @@ var CompanyList = React.createClass({
             });
     },
 
+    _handleFilterChange(filterBy, val) {
+        this.setState({
+            companies: this.state.companies.map(c => {
+                c.hidden = val && !c[filterBy].toLowerCase().startsWith(val);
+                return c;
+            }),
+        });
+    },
+
     render() {
         let {companies, companiesLoaded} = this.state;
 
@@ -44,12 +53,79 @@ var CompanyList = React.createClass({
             <div className='container companies'>
                 <b>Companies:</b>
                 <Loader loaded={companiesLoaded}>
+                    <CompanyFilter onChange={this._handleFilterChange} />
                     <ListGroup>
-                        {companies.map(c => {
+                        {companies.filter(c => {
+                            return !c.hidden;
+                        }).map(c => {
                             return <CompanyListItem company={c} key={c.id} />;
                         })}
                     </ListGroup>
                 </Loader>
+            </div>
+        );
+    }
+});
+
+
+/* A simple filter component with an input and a filter type selector. */
+var CompanyFilter = React.createClass({
+    propTypes: {
+        onChange: React.PropTypes.func.isRequired,
+    },
+
+    getInitialState() {
+        return {
+            filterBy: 'name',
+            inputVal: '',
+        };
+    },
+
+    _handleInputChange(e) {
+        this.setState({
+            inputVal: e.target.value,
+        });
+        this.props.onChange(this.state.filterBy, e.target.value.trim());
+    },
+
+    _handleTypeChange(newType) {
+        this.setState({
+            filterBy: newType,
+            inputVal: '',
+        });
+
+        this.props.onChange(newType, '');
+    },
+
+    render() {
+        let {filterBy, inputVal} = this.state;
+        let filterTypes = ['name', 'country', 'sector'];
+
+        let typeSelect = (
+            <DropdownButton title={filterBy}>
+                {filterTypes.map(f => {
+                    return (
+                        <MenuItem
+                            key={f}
+                            eventKey={f}
+                            active={f === filterBy}
+                            onSelect={this._handleTypeChange}>
+                            {f}
+                        </MenuItem>
+                    );
+                })}
+            </DropdownButton>
+        );
+
+        return (
+            <div>
+                <Input
+                    type='text'
+                    placeholder='Filter companies'
+                    ref='input'
+                    buttonAfter={typeSelect}
+                    value={inputVal}
+                    onChange={this._handleInputChange} />
             </div>
         );
     }
@@ -67,7 +143,7 @@ var CompanyListItem = React.createClass({
         return (
             <Link to='companyDetail' params={{companyId: c.id}}>
                 <ListGroupItem header={c.name}>
-                    {c.revenue} {c.country} {c.sector}
+                    {c.revenue} {c.country} {c.sector} {c.hidden || ''}
                 </ListGroupItem>
             </Link>
         );
